5554964268a047fb99006e16e8e6a8a730d353da
dereksong01 <dsong1@stuy.edu>
2019-02-06 00:27:48 -0500

    k02 done

--- 02_canvas/dotconn.js
+++ 02_canvas/dotconn.js
@@ -10,23 +10,22 @@ var clrbut = document.getElementById("clear");
 
 var clear = function() {
     ctx.clearRect(0, 0, c.width, c.height);
-    ctx.closePath();
     ctx.beginPath();
 }
 
 var dot = function(x, y) {
-    ctx.fillStyle = "blue";
-    ctx.ellipse(x,y,7,7,0,0,2 * Math.PI);
-    //ctx.moveTo(x, y);
+    ctx.fillStyle = "#FF0000";
+    ctx.arc(x,y,7,0,2 * Math.PI);
+    ctx.fill();
+    ctx.moveTo(x, y);
     //ctx.lineTo(x, y);
     ctx.stroke();
-    //ctx.moveTo(x, y);
 }
 
 var draw = function(e) {
     var x = e.offsetX; //The value of the x coordinate relative to the canvas (upper left corner)
     var y = e.offsetY; //The value of the y coordinate relative to the canvas (upper left corner)
-	  dot(x,y);
+    dot(x,y);
 }
 
 clrbut.addEventListener('click', clear);
