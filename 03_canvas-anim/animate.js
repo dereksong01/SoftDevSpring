0f7cbc032cc82d318da3ecc1d3434f29ea1f4aea
dereksong01 <dsong1@stuy.edu>
2019-02-06 00:37:47 -0500

    k03

---
+++ 03_canvas-anim/animate.js
@@ -0,0 +1,71 @@
+// Derek Song
+// SoftDev2 - pd8
+// K03 -- They lock us in the tower whenever we get caught
+// 2019 - 02 - 05
+
+// Code adapted from Rubin Peci
+// Did not know how to do and it is currently the forbidden hours, thus, I used Rubin's code.
+var canvas = document.getElementById("playground");
+var ctx = canvas.getContext("2d");
+var requestID;
+var radius = 1;
+var growing = true;
+
+var init = function(e)
+{
+	if (requestID != null)
+	{
+		e.preventDefault();
+		console.log("Already animating, this button does NOTHING");
+	}
+	else
+	{
+		console.log("Starting animation!");
+		requestID = window.requestAnimationFrame(drawDot);
+	}
+};
+
+var drawDot = function()
+{
+	if ( radius >= canvas.width / 2 || radius <= 0)
+	{
+		growing = !growing
+	}
+
+	if (growing)
+	{
+		radius++;
+	}
+	else
+	{
+		radius--;
+	}
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	ctx.fillStyle = "red";
+	ctx.beginPath();
+	ctx.ellipse(canvas.width / 2, canvas.height / 2, radius, radius, 0, 0, 2 * Math.PI);
+	ctx.stroke;
+	ctx.fill();
+
+	requestID = window.requestAnimationFrame(drawDot);
+};
+
+var stopIt = function(e)
+{
+	if (requestID == null)
+	{
+		e.preventDefault();
+		console.log("Animation already stopped, this button does nothing!");
+	}
+	else
+	{
+		console.log("request ID is " + requestID + ", animation has been STOPPED");
+		window.cancelAnimationFrame(requestID);
+		requestID = null;
+  }
+};
+
+document.getElementById('circle').addEventListener('click', init);
+document.getElementById('stop').addEventListener('click', stopIt);
